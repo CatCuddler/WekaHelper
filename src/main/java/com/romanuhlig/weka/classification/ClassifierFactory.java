f9f2e1a3fb9833b6a9d29127724451eebf8c9cc7
Roman <>
2019-03-27 15:53:35 +0100

    refactoring and comments

--- src/main/java/com/romanuhlig/weka/classification/ClassifierFactory.java
+++ src/main/java/com/romanuhlig/weka/classification/ClassifierFactory.java
@@ -1,27 +1,24 @@
 package com.romanuhlig.weka.classification;
 
-import weka.attributeSelection.*;
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.BayesNet;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.functions.*;
-import weka.classifiers.functions.supportVector.NormalizedPolyKernel;
-import weka.classifiers.functions.supportVector.Puk;
-import weka.classifiers.functions.supportVector.RBFKernel;
-import weka.classifiers.functions.supportVector.StringKernel;
 import weka.classifiers.lazy.IBk;
 import weka.classifiers.lazy.KStar;
 import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.meta.MultiClassClassifier;
 import weka.classifiers.rules.*;
 import weka.classifiers.trees.*;
-import weka.core.SelectedTag;
-import weka.core.WekaEnumeration;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Produces Weka compatible classifiers
+ *
+ * @author Roman Uhlig
+ */
 public class ClassifierFactory {
 
     public enum ClassifierType {
@@ -31,6 +28,12 @@ public class ClassifierFactory {
     }
 
 
+    /**
+     * Returns a list of classifiers based on the requested types
+     *
+     * @param classifierTypes
+     * @return
+     */
     public ArrayList<Classifier> getClassifiers(List<ClassifierType> classifierTypes) {
 
         ArrayList<Classifier> classifiers = new ArrayList<>();
@@ -124,210 +127,326 @@ public class ClassifierFactory {
         return classifiers;
     }
 
+    /**
+     * The J48 classifier
+     *
+     * @return
+     */
     private Classifier getJ48() {
         Classifier classifier = new J48();
         return classifier;
     }
 
+    /**
+     * The Naive Bayes classifier
+     *
+     * @return
+     */
     private Classifier getNaiveBayes() {
         Classifier classifier = new NaiveBayes();
         return classifier;
     }
 
+    /**
+     * The Random Forest classifier
+     *
+     * @return
+     */
     private Classifier getRandomForest() {
         Classifier classifier = new RandomForest();
         return classifier;
     }
 
+    /**
+     * The ZeroR classifier
+     *
+     * @return
+     */
     private Classifier getZeroR() {
         Classifier classifier = new ZeroR();
         return classifier;
     }
 
+    /**
+     * The OneR classifier
+     *
+     * @return
+     */
     private Classifier getOneR() {
         Classifier classifier = new OneR();
         return classifier;
     }
 
+    /**
+     * The SMO classifier
+     *
+     * @return
+     */
     private Classifier getSMO() {
         SMO classifier = new SMO();
-
-        // fit calibration models to the SVMs output (for proper probability estimates)
-//        classifier.setBuildCalibrationModels(true);
-
-
-        // npk slight downgrade, a bit slower
-        // RBF slower ~x15, better on lowest two but worse on 4 others, only ALL_2 instead of ALL_3
-        // Puk very slow
-//        classifier.setKernel(new RBFKernel());
-
-//        classifier.setNumDecimalPlaces(8);
-//        try {
-//            classifier.setOptions(new String[]{"-C", "0.5"});
-//        } catch (Exception e) {
-//            System.out.println("unable to set options for SMO classifier");
-//        }
 
         return classifier;
     }
 
+    /**
+     * The SMO classifier with additional built in feature selection
+     * Does not work with the thesis data set
+     *
+     * @return
+     */
     private Classifier getSMOfeatureSelected() {
         AttributeSelectedClassifier asClassifier = new AttributeSelectedClassifier();
         asClassifier.setClassifier(getSMO());
-//        GreedyStepwise search = new GreedyStepwise();
-//        search.setNumToSelect(50);
-//        asClassifier.setSearch(search);
 
-//        WrapperSubsetEval evaluator = new WrapperSubsetEval();
-//        evaluator.setClassifier(getSMO());
-//        asClassifier.setEvaluator(evaluator);
+        // potential settings:
 
-//        ConsistencySubsetEval evaluator = new ConsistencySubsetEval();
-//        asClassifier.setEvaluator(evaluator);
+        // GreedyStepwise search = new GreedyStepwise();
+        // search.setNumToSelect(50);
+        // asClassifier.setSearch(search);
 
-//        GainRatioAttributeEval evaluator = new GainRatioAttributeEval();
-//        asClassifier.setEvaluator(evaluator);
-//        Ranker search = new Ranker();
-//        search.setNumToSelect(50);
-//        asClassifier.setSearch(search);
+        // WrapperSubsetEval evaluator = new WrapperSubsetEval();
+        // evaluator.setClassifier(getSMO());
+        // asClassifier.setEvaluator(evaluator);
 
-//        OneRAttributeEval evaluator = new OneRAttributeEval();
-//        asClassifier.setEvaluator(evaluator);
-//        Ranker search = new Ranker();
-//        search.setNumToSelect(50);
-//        asClassifier.setSearch(search);
+        // ConsistencySubsetEval evaluator = new ConsistencySubsetEval();
+        // asClassifier.setEvaluator(evaluator);
+
+        // GainRatioAttributeEval evaluator = new GainRatioAttributeEval();
+        // asClassifier.setEvaluator(evaluator);
+        // Ranker search = new Ranker();
+        // search.setNumToSelect(50);
+        // asClassifier.setSearch(search);
+
+        // OneRAttributeEval evaluator = new OneRAttributeEval();
+        // asClassifier.setEvaluator(evaluator);
+        // Ranker search = new Ranker();
+        // search.setNumToSelect(50);
+        // asClassifier.setSearch(search);
 
         return asClassifier;
     }
 
+    /**
+     * The Decision Table classifier
+     *
+     * @return
+     */
     private Classifier getDecisionTable() {
         Classifier classifier = new DecisionTable();
         return classifier;
     }
 
+    /**
+     * The Gaussian Process classifier
+     *
+     * @return
+     */
     private Classifier getGaussianProcess() {
         Classifier classifier = new GaussianProcesses();
         return classifier;
     }
 
+    /**
+     * The M5P classifier
+     *
+     * @return
+     */
     private Classifier getM5P() {
         Classifier classifier = new M5P();
         return classifier;
     }
 
+    /**
+     * The KStar classifier
+     *
+     * @return
+     */
     private Classifier getKStar() {
         Classifier classifier = new KStar();
         return classifier;
     }
 
+    /**
+     * The LMT classifier
+     *
+     * @return
+     */
     private Classifier getLMT() {
         Classifier classifier = new LMT();
         return classifier;
     }
 
+    /**
+     * The Bayes Net classifier
+     *
+     * @return
+     */
     private Classifier getBayesNet() {
         Classifier classifier = new BayesNet();
         return classifier;
     }
 
+    /**
+     * The JRip classifier
+     *
+     * @return
+     */
     private Classifier getJRip() {
         Classifier classifier = new JRip();
         return classifier;
     }
 
+    /**
+     * The Simple Logistic classifier
+     *
+     * @return
+     */
     private Classifier getSimpleLogistic() {
         Classifier classifier = new SimpleLogistic();
         return classifier;
     }
 
+    /**
+     * The Linear Regression classifier
+     *
+     * @return
+     */
     private Classifier getLinearRegression() {
         Classifier classifier = new LinearRegression();
         return classifier;
     }
 
+    /**
+     * The Voted Perceptron classifier
+     *
+     * @return
+     */
     private Classifier getVotedPerceptron() {
         MultiClassClassifier classifier = new MultiClassClassifier();
         classifier.setClassifier(new VotedPerceptron());
         return classifier;
     }
 
+    /**
+     * The SGD classifier
+     *
+     * @return
+     */
     private Classifier getSGD() {
         MultiClassClassifier classifier = new MultiClassClassifier();
         classifier.setClassifier(new SGD());
         return classifier;
     }
 
+    /**
+     * The Logistic classifier
+     *
+     * @return
+     */
     private Classifier getLogistic() {
         Classifier classifier = new Logistic();
         return classifier;
     }
 
+    /**
+     * The Multilayer Perceptron classifier
+     *
+     * @return
+     */
     private Classifier getMultilayerPerceptron() {
         Classifier classifier = new MultilayerPerceptron();
         return classifier;
     }
 
+    /**
+     * The RepTree classifier
+     *
+     * @return
+     */
     private Classifier getREPTree() {
         Classifier classifier = new REPTree();
         return classifier;
     }
 
+    /**
+     * The IBK classifier
+     *
+     * @return
+     */
     private Classifier getIBk() {
         Classifier classifier = new IBk();
         return classifier;
     }
 
+    /**
+     * The Random Tree classifier
+     *
+     * @return
+     */
     private Classifier getRandomTree() {
         Classifier classifier = new RandomTree();
         return classifier;
     }
 
+    /**
+     * The SMOreg classifier
+     *
+     * @return
+     */
     private Classifier getSMOreg() {
         Classifier classifier = new SMOreg();
         return classifier;
     }
 
 
+    /**
+     * The LibSVM classifier wrapper
+     *
+     * @return
+     */
     private Classifier getLibSVM() {
-    	// removed for now due to maven build issues if required repository is included
-//        LibSVM libSVM = new LibSVM();
-//        libSVM.setNormalize(true);
-//        libSVM.setEps(0.000000000001);
-//        libSVM.setSVMType(new SelectedTag(LibSVM.SVMTYPE_NU_SVC, LibSVM.TAGS_SVMTYPE));
-//        libSVM.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_LINEAR, LibSVM.TAGS_KERNELTYPE));
-//        libSVM.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_POLYNOMIAL, LibSVM.TAGS_KERNELTYPE));
-//        libSVM.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_SIGMOID, LibSVM.TAGS_KERNELTYPE));
+        // removed for now due to maven build issues if the required repository is included
+        System.out.println("LibSVM removed for now due to maven build issues if required repository is included");
 
+        // potential settings:
 
-        // normalize: 2-3h, 0.92 - ALL_1 (lLeg-rForearm)
-        // normalize, linear kernel: 10min, 0.93-ALL_2 (identical to SMO results)
-        // normalize, polynomial kernel: expected 15 hours, ~.35 in first two test subject, aborted
-        // normalize, nu, eps=0.000000000001: over-night-long, ~.82 in first two complete tests, aborted
-        // normalize, sigmoid kernel: 3h, .91-All_1
+        // LibSVM libSVM = new LibSVM();
+        // libSVM.setNormalize(true);
+        // libSVM.setEps(0.000000000001);
+        // libSVM.setSVMType(new SelectedTag(LibSVM.SVMTYPE_NU_SVC, LibSVM.TAGS_SVMTYPE));
+        // libSVM.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_LINEAR, LibSVM.TAGS_KERNELTYPE));
+        // libSVM.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_POLYNOMIAL, LibSVM.TAGS_KERNELTYPE));
+        // libSVM.setKernelType(new SelectedTag(LibSVM.KERNELTYPE_SIGMOID, LibSVM.TAGS_KERNELTYPE));
 
- //       return libSVM;
-        
-    	return new ZeroR();
+        // return libSVM;
+
+        return new ZeroR();
 
     }
 
+    /**
+     * The LibLinear classifier wrapper
+     *
+     * @return
+     */
     private Classifier getLibLinear() {
-    	// removed for now due to maven build issues if required repository is included
+        // removed for now due to maven build issues if required repository is included
+        System.out.println("LibLinear removed for now due to maven build issues if required repository is included");
 
-//        LibLINEAR libLinear = new LibLINEAR();
-//        libLinear.setNormalize(true);
-//        String[] options = {"-S", "4", "-C", "1.0", "-E", "0.001", "-B 1.0", "-Z", "-L", "0.1", "-I", "1000"};
-//        try {
-//            libLinear.setOptions(options);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        // potential settings:
 
-        // normalize, Cramer/Singer SVM: 13min, .91-.99
-        // normalize: 10min, .84-ALL_1
+        // LibLINEAR libLinear = new LibLINEAR();
+        // libLinear.setNormalize(true);
+        // String[] options = {"-S", "4", "-C", "1.0", "-E", "0.001", "-B 1.0", "-Z", "-L", "0.1", "-I", "1000"};
+        // try {
+        //     libLinear.setOptions(options);
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
+
 
 //    	return libLinear;
-    	return new ZeroR();
+        return new ZeroR();
     }
 
 }
