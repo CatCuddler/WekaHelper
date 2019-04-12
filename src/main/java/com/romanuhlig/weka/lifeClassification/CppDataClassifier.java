77645be94042400e46ef5186b4296fc86287d45f
Roman <>
2019-04-12 11:15:16 +0200

    refactoring and comments for "CppDataClassifier"

--- src/main/java/com/romanuhlig/weka/lifeClassification/CppDataClassifier.java
+++ src/main/java/com/romanuhlig/weka/lifeClassification/CppDataClassifier.java
@@ -17,18 +17,31 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Responsible for communicating with a C++ environment through the JNI interface
+ * <p>
+ * Loads a pre-trained model to regularly predict the class of incoming sensor readings
+ *
+ * @author Roman Uhlig
+ */
 public class CppDataClassifier {
 
+    // time when the last sensor reading was registered, in seconds
     double timeOfLastFrameData = 0;
-    double timeOfLastClassification = 0;
 
+    // time of the last prediction attempt, in seconds
+    double timeOfLastClassification = 0;
+    // pause between two prediction attempts, in seconds
     final double timeBetweenClassifications = 1.2;
 
+    // collector for incoming sensor readings
     private FrameDataSet frameDataSet;
 
+    // pre-trained model
     final String pathToClassifier;
     Classifier classifier;
 
+    // the header for all instances, generated the first time it is needed
     ArrayList<String> instanceHeader;
 
     // data required for any classifier and any run
@@ -37,11 +50,17 @@ public class CppDataClassifier {
     ArrayList<Attribute> attributes;
     ExecutorService threadPool = Executors.newCachedThreadPool();
 
+    /**
+     * Creates a new CppDataClassifier
+     * <p>
+     * Requires a wekaModel.model file within the same folder as the jar file
+     */
     public CppDataClassifier() {
+
         frameDataSet = new FrameDataSet("", "");
 
-        // load pre-trained weka model
-        pathToClassifier = getFilePathToJar() + "/wekaModel.model";
+        // load pre-trained weka model from the same folder as the jar file
+        pathToClassifier = getFolderPathToJar() + "/wekaModel.model";
         try {
             classifier = (Classifier) weka.core.SerializationHelper.read(pathToClassifier);
             outputClassifierResultToCpp("weka model successfully loaded from file");
@@ -49,9 +68,8 @@ public class CppDataClassifier {
             outputClassifierResultToCpp("unable to load weka classifier from   " + pathToClassifier + "   !!!");
         }
 
-
+        // initialize class names
         classVal = new ArrayList<>();
-        // add class attribute
         classVal.add("jogging");
         classVal.add("kick");
         classVal.add("kickPunch");
@@ -65,6 +83,30 @@ public class CppDataClassifier {
         classAttribute = new Attribute("activity", classVal);
     }
 
+    /**
+     * Add a new sensor reading, and predict the class of recent readings if enough time has passed
+     * <p>
+     * Meant to be called by a C++ environment through JNI
+     *
+     * @param sensorPosition
+     * @param subject
+     * @param activity
+     * @param calPosX
+     * @param calPosY
+     * @param calPosZ
+     * @param calRotX
+     * @param calRotY
+     * @param calRotZ
+     * @param calRotW
+     * @param angVelX
+     * @param angVelY
+     * @param angVelZ
+     * @param linVelX
+     * @param linVelY
+     * @param linVelZ
+     * @param scale
+     * @param time
+     */
     public void addFrameData(String sensorPosition, String subject, String activity,
                              double calPosX, double calPosY, double calPosZ,
                              double calRotX, double calRotY, double calRotZ, double calRotW,
@@ -72,79 +114,48 @@ public class CppDataClassifier {
                              double linVelX, double linVelY, double linVelZ,
                              double scale, double time) {
 
-
-//        if (frameDataSet.getAllSensorLists().size() > 0) {
-//            if (frameDataSet.getAllSensorLists().get(0) != null) {
-//                outputClassifierResultToCpp("" + frameDataSet.getAllSensorLists().get(0).size());
-//            }
-//        }
-
-//        outputClassifierResultToCpp("" + time);
-
         // only consider a new classification once a new frame has started
         // that is, not while different sensors for the current frame might still be incoming
         if ((time != timeOfLastFrameData) && (time - timeOfLastClassification > timeBetweenClassifications)) {
 
-//            outputClassifierResultToCpp("enough time passed");
-
-            // only consider a new classification if enough data was collected
+            // if enough data was collected, start a new classification attempt
             if (frameDataSet.enoughDataForWindowSize(TestBenchSettings.getWindowSizeForFrameDataToFeatureConversion())) {
-
-//                outputClassifierResultToCpp("enough data collected, trying classification");
 
                 timeOfLastClassification = time;
 
-//                outputClassifierResultToCpp("a");
-
-
-//                StopWatch fullProcessStopWatch = new StopWatch();
-//                fullProcessStopWatch.start();
-
+                // copy the data required for a classification attempt, and get rid of older data
                 FrameDataSet frameDataSetForWindow =
                         frameDataSet.getLatestDataForWindowSizeAndRemoveEarlierData(
                                 TestBenchSettings.getWindowSizeForFrameDataToFeatureConversion());
-
-
-//                outputClassifierResultToCpp("b");
-
 
                 // run the feature computation and execution within a separate thread, to avoid slowdowns
                 Runnable recognitionThread = new Runnable() {
                     public void run() {
 
-
-//                        outputClassifierResultToCpp("starting classification thread");
-
                         // create features
                         FeatureVector features = FeatureExtractor.getFeaturesForFrameDataSet(frameDataSetForWindow);
 
-                        // create header and attribute types
+                        // if this is the first classification attempt, create header and attribute types
                         if (instanceHeader == null) {
+
+                            // header
                             instanceHeader = FeatureExtractor.getFeatureHeaderForFrameDataSet(frameDataSetForWindow, false, false);
 
-                            //                        outputClassifierResultToCpp("c");
-
-                            // create weka data instance from features
-                            // ---------------- Instance creation -------------------------
-                            // Create empty instance with three attribute values
+                            // attribute types
                             int numberOfFeatures = instanceHeader.size();
                             attributes = new ArrayList<>(numberOfFeatures);
                             for (int i = 0; i < numberOfFeatures; i++) {
                                 Attribute newAttribute = new Attribute(instanceHeader.get(i));
                                 attributes.add(newAttribute);
-//                              outputClassifierResultToCpp("attribute added:   " + header.get(i));
                             }
                             attributes.add(classAttribute);
-
-
                         }
 
-
+                        // create a new instance
                         int numberOfFeatures = instanceHeader.size();
                         Instance instance = new DenseInstance(numberOfFeatures);
-//                        outputClassifierResultToCpp("d");
 
-                        // create an Instances Object to house the instance (as its single entry)
+                        // create an InstanceS Object to house the Instance (as its single entry)
                         Instances instances = new Instances("LifeInstances", attributes, 0);
                         instances.setClass(classAttribute);
 
@@ -152,69 +163,33 @@ public class CppDataClassifier {
                         instances.add(instance);
                         instance.setDataset(instances);
 
-//                        outputClassifierResultToCpp("e");
-
+                        // create the feature values for the instance
                         ArrayList<Double> featureValues = features.getFeaturesWithoutClassAndSubject();
 
-                        // fill the attribute values for the instance
+                        // fill the attribute values for the instance with the feature values
                         for (int i = 0; i < numberOfFeatures; i++) {
-//                    outputClassifierResultToCpp("trying to set value:   ");
-//                    outputClassifierResultToCpp("" + features.getFeaturesWithoutClassAndSubject().get(i));
-//                    outputClassifierResultToCpp("" + attributes.get(i).name());
-
-
                             try {
-//                                instance.setValue(attributes.get(i), featureValues.get(i));
                                 instance.setValue(i, featureValues.get(i));
-                                // outputClassifierResultToCpp("success setting value:   " + i + "   " + attributes.get(i).name());
                             } catch (Exception e) {
                                 outputClassifierResultToCpp(e.getLocalizedMessage());
                                 outputClassifierResultToCpp("error setting value:   " + i + "   " + attributes.get(i).name());
                             }
                         }
 
-
-//                        outputClassifierResultToCpp("f");
-
-
-////              output state for debugging
-//                outputClassifierResultToCpp("final attributes in instance:");
-//                for (int i = 0; i < instance.numAttributes(); i++) {
-//                    outputClassifierResultToCpp(instance.attribute(i).name() + "   " + instance.value(i));
-//                }
-
-//                        outputClassifierResultToCpp("gonna start actual classification");
-
-
-                        // predict the class
+                        // predict the class of the created instance
                         try {
                             double prediction = classifier.classifyInstance(instance);
-                            // outputClassifierResultToCpp("prediction:   " + prediction + "   " + classVal.get((int) prediction));
                             outputClassifierResultToCpp(classVal.get((int) prediction));
                         } catch (Exception e) {
                             outputClassifierResultToCpp(e.getLocalizedMessage());
                             outputClassifierResultToCpp("classification failed !!!");
                         }
-
-//                        outputClassifierResultToCpp("g");
-
-//                outputClassifierResultToCpp("number of sensor lists = " + frameDataSetForWindow.getAllSensorLists().size());
-//                outputClassifierResultToCpp("first list data = " + frameDataSetForWindow.getAllSensorLists().get(0).size());
-//                outputClassifierResultToCpp("classifier Result = " + frameDataSet.getAllSensorLists().get(0).size());
-//                outputClassifierResultToCpp(sensorPosition);
-
-//                        fullProcessStopWatch.stop();
-//                        outputClassifierResultToCpp("total time:   " + fullProcessStopWatch.getTime(TimeUnit.MILLISECONDS));
                     }
-
                 };
 
                 threadPool.execute(recognitionThread);
-
             }
-
         }
-
 
         // create and remember new frame data
         FrameData newFrameData = new FrameData(
@@ -227,34 +202,14 @@ public class CppDataClassifier {
         frameDataSet.addFrameData(newFrameData);
 
         timeOfLastFrameData = newFrameData.getTime();
-
-
-// ---------------- Instance creation -------------------------
-//        // Create empty instance with three attribute values
-//        Instance inst = new DenseInstance(3);
-//        Attribute length = new Attribute("length");
-//        Attribute height = new Attribute("height");
-//        Attribute width = new Attribute("width");
-//
-//        Classifier classifier;
-//
-//// Set instance's values for the attributes "length", "weight", and "position"
-//        inst.setValue(length, 5.3);
-//        inst.setValue(height, 300);
-//        inst.setValue(width, "first");
-//
-//// Set instance's dataset to be the dataset "race"
-//        inst.setDataset(inst);
-//
-//// Print the instance
-//        System.out.println("The instance: " + inst);
-//
-//        classifier.classifyInstance(inst);
-
-
     }
 
-    String getFilePathToJar() {
+    /**
+     * Determine the folder path of the jar file containing this class
+     *
+     * @return
+     */
+    String getFolderPathToJar() {
 
         String path = CppDataClassifier.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         try {
@@ -267,8 +222,12 @@ public class CppDataClassifier {
         }
     }
 
-    // to be supplied in C++ through JNI
+    /**
+     * A callback function to output results to a C++ environment through the JNI interface
+     * <p>
+     * Meant to be set by the C++ environment
+     *
+     * @param result
+     */
     public native void outputClassifierResultToCpp(String result);
-
-
 }
