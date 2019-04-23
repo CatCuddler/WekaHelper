439f7301d45f916dc4a4e455478334ff22f52875
Roman <>
2019-04-23 14:52:58 +0200

    comments and cleanup for FileWriter

--- src/main/java/com/romanuhlig/weka/io/FileWriter.java
+++ src/main/java/com/romanuhlig/weka/io/FileWriter.java
@@ -10,73 +10,74 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Writes data to disk
+ *
+ * @author Roman Uhlig
+ */
 public class FileWriter {
 
-
+    /**
+     * Write a table containing a single classification result to disk
+     *
+     * @param result
+     * @param folder
+     * @param filename
+     */
     public static void writeClassificationResult(ClassificationResult result, String folder, String filename) {
 
         ensureFolderExists(folder);
 
+        // forward request to writer for multiple results
         List<ClassificationResult> results = new ArrayList<>();
         results.add(result);
-
         writeClassificationResults(results, folder, filename);
-
     }
-//
-//    public static void writeClassificationResults(List<ClassificationResult> results, String folder, String
-//            filename) {
-//
-//        // make sure folder exists
-//        new File(folder).mkdirs();
-//
-//        String fullFilePath = folder + filename + ".csv";
-//
-//        try (
-//                Writer writer = Files.newBufferedWriter(Paths.get(fullFilePath));
-//        ) {
-//            StatefulBeanToCsv<ClassificationResult> beanToCsv = new StatefulBeanToCsvBuilder(writer)
-//                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
-//                    .build();
-//
-//            beanToCsv.write(results);
-//
-//        } catch (Exception e) {
-//            System.err.println("could not write classification result " + fullFilePath);
-//        }
-//
-//    }
 
-    public static void writeClassificationResults(List<ClassificationResult> results, String folder, String
-            filename) {
+    /**
+     * Write table containing the given classification results to disk
+     *
+     * @param results
+     * @param folder
+     * @param filename
+     */
+    public static void writeClassificationResults(
+            List<ClassificationResult> results, String folder, String filename) {
 
         ensureFolderExists(folder);
 
         String fullFilePath = folder + filename + ".csv";
 
         try (
+                // create table writer
                 Writer writer = Files.newBufferedWriter(Paths.get(fullFilePath));
-
                 CSVWriter csvWriter = new CSVWriter(writer,
                         CSVWriter.DEFAULT_SEPARATOR,
                         CSVWriter.NO_QUOTE_CHARACTER,
                         CSVWriter.DEFAULT_ESCAPE_CHARACTER,
                         CSVWriter.DEFAULT_LINE_END);
         ) {
-
+            // add header
             csvWriter.writeNext(ClassificationResult.getHeaderForCSV());
 
+            // writer results line by line
             for (ClassificationResult result : results) {
                 csvWriter.writeNext(result.getDataForCSV());
             }
-
         } catch (Exception e) {
             System.err.println("could not write classification result " + fullFilePath);
         }
-
     }
 
+    /**
+     * Write the given string to a simple text file
+     *
+     * @param text
+     * @param folder
+     * @param filename
+     */
     public static void writeTextFile(String text, String folder, String filename) {
+
         ensureFolderExists(folder);
 
         File logFile = new File(folder + filename);
@@ -85,39 +86,57 @@ public class FileWriter {
                 BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(logFile));
         ) {
             writer.write(text);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
+    /**
+     * Write the names of the features used for the training and test data to disk
+     *
+     * @param trainingData
+     * @param testData
+     * @param folder
+     * @param filename
+     */
     public static void writeFeaturesUsed(Instances trainingData, Instances testData, String folder, String filename) {
 
         ensureFolderExists(folder);
 
         StringBuilder stringBuilder = new StringBuilder();
 
+        // collect training data features
         stringBuilder.append("Training Data features:   ");
         for (int i = 0; i < trainingData.numAttributes(); i++) {
             stringBuilder.append(trainingData.attribute(i).name());
             stringBuilder.append("   ");
         }
 
+        // formatting
         stringBuilder.append(System.lineSeparator());
         stringBuilder.append(System.lineSeparator());
 
+        // collect test data features
         stringBuilder.append("Test Data features:       ");
-        for (int i = 0; i < trainingData.numAttributes(); i++) {
-            stringBuilder.append(trainingData.attribute(i).name());
+        for (int i = 0; i < testData.numAttributes(); i++) {
+            stringBuilder.append(testData.attribute(i).name());
             stringBuilder.append("   ");
         }
 
         writeTextFile(stringBuilder.toString(), folder, filename);
-
     }
 
-    public static void writeNewFeatureExtractionResults(SubjectsFeatureExtractionResults results, String currentFolder, String existingFeaturesInputFolder, String filename) {
+    /**
+     * Delete all files within the existing features extraction result folder,
+     * and place a new feature extraction result file within both folders
+     *
+     * @param results
+     * @param currentFolder
+     * @param existingFeaturesInputFolder
+     * @param filename
+     */
+    public static void writeNewFeatureExtractionResults(SubjectsFeatureExtractionResults results, String currentFolder,
+                                                        String existingFeaturesInputFolder, String filename) {
 
         // get rid of any existing feature extraction result
         File inputFolder = new File(existingFeaturesInputFolder);
@@ -128,13 +147,20 @@ public class FileWriter {
             }
         }
 
-
         // save new file
-        serializeObject(results, currentFolder, filename);
-        serializeObject(results, existingFeaturesInputFolder, filename);
+        serializeFeatureExtractionResult(results, currentFolder, filename);
+        serializeFeatureExtractionResult(results, existingFeaturesInputFolder, filename);
     }
 
-    private static void serializeObject(SubjectsFeatureExtractionResults results, String folder, String filename) {
+    /**
+     * Write a serialized version of the given feature extraction results to disk
+     *
+     * @param results
+     * @param folder
+     * @param filename
+     */
+    private static void serializeFeatureExtractionResult(
+            SubjectsFeatureExtractionResults results, String folder, String filename) {
 
         ensureFolderExists(folder);
 
@@ -145,18 +171,27 @@ public class FileWriter {
             objectOutputStream.flush();
             objectOutputStream.close();
         } catch (Exception e) {
-            System.out.println("unable to write serialize object");
+            System.out.println("unable to write serialized feature extraction results");
         }
-
     }
 
-
+    /**
+     * Load serialized feature extraction results from disk
+     * <p>
+     * Assumes that there is exactly one file within the given folder, which is a serialized version
+     * of previously created feature extraction results
+     *
+     * @param existingFeaturesInputFolder
+     * @return
+     */
     public static SubjectsFeatureExtractionResults readExistingFeatureSet(String existingFeaturesInputFolder) {
 
+        // get all files within the given folder
         File inputFolder = new File(existingFeaturesInputFolder);
         File[] listOfInputFiles = inputFolder.listFiles();
 
         try {
+            // try to load the first file (there should be only one)
             FileInputStream fileInputStream = new FileInputStream(listOfInputFiles[0].getPath());
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             SubjectsFeatureExtractionResults result = (SubjectsFeatureExtractionResults) objectInputStream.readObject();
@@ -167,11 +202,14 @@ public class FileWriter {
             e.printStackTrace();
             return new SubjectsFeatureExtractionResults(new ArrayList<>());
         }
-
-
     }
 
-    private static void ensureFolderExists(String folder){
+    /**
+     * Create the given folder, if it does not exist yet
+     *
+     * @param folder
+     */
+    private static void ensureFolderExists(String folder) {
         new File(folder).mkdirs();
     }
 }
